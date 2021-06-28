b8988b7b3b2ec0b2ca15bbc608d64afbbbb8d71c
b1mind <brent@1mindsolutions.com>
2021-06-27 19:26:54 -0600

    first try deploy?

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -72,7 +72,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "de-DE"
+LANGUAGE_CODE = "en-EN"
 
 TIME_ZONE = "CET"
 
